7db44a365c91790980b4f3c826875215cfea65a1
GardenBeast <gardenbeast2007@gmail.com>
2025-04-28 21:20:11 +0300

    вввв

--- collect_files.sh
+++ collect_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
 input_dir="$1"
 output_dir="$2"
 
-find "$input_dir" -type f | while read -r filepath; do
-    relative_path="${filepath#$input_dir/}"
-    safe_name=$(echo "$relative_path" | tr '/' '_')
-    cp "$filepath" "$output_dir/$safe_name"
-done
+find "$input_dir" -type f -exec cp {} "$output_dir/" \;
